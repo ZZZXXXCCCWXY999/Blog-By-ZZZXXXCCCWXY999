20149cd15971571e46022b0225704b14adebbc23
ZZZXXXCCCWXY999 <zzzxxxcccwxy999@gmail.com>
2020-01-31 16:01:53 +0800

    Compile the gradle

---
+++ src/test/java/xyz/zxcwxy999/blog/BlogByZzzxxxcccwxy999ApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.zxcwxy999.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogByZzzxxxcccwxy999ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
